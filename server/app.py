bb4fed83aba668501f26fe8025b427f8f92fc135
joseph <michealkinuthia.m.k@gmail.com>
2024-04-21 20:11:15 +0300

    CRUD

--- server/app.py
+++ server/app.py
@@ -26,5 +26,179 @@ def index():
     return "<h1>Code challenge</h1>"
 
 
+
+class Pizzas(Resource):
+
+    def get(self):
+
+        response_dict_list = [pizza.serialize() for pizza in Pizza.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200,
+        )
+
+        return response
+
+    def post(self):
+
+        new_record = Pizza(
+            title=request.form['title'],
+            body=request.form['body'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            response_dict,
+            201,
+        )
+
+        return response
+
+api.add_resource(Pizzas, '/pizzas')
+
+
+
+class PizzaByID(Resource):
+    
+
+    def get(self, id):
+
+        response_dict = Pizza.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+
+    def patch(self, id):
+
+        record = Pizza.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+    def delete(self, id):
+
+        record = Pizza.query.filter_by(id=id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(PizzaByID, '/pizzas/<int:id>')
+
+class Restaurants(Resource):
+
+    def get(self):
+
+        response_dict_list = [restaurant.to_dict() for restaurant in Restaurant.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200,
+        )
+
+        return response
+
+    def post(self):
+
+        new_record = Restaurant(
+            title=request.form['title'],
+            body=request.form['body'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            response_dict,
+            201,
+        )
+
+        return response
+
+api.add_resource(Restaurants, '/restaurants')
+
+
+class RestaurantByID(Resource):
+    
+
+    def get(self, id):
+
+        response_dict = Restaurant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+
+    def patch(self, id):
+
+        record = Restaurant.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+    def delete(self, id):
+
+        record = Restaurant.query.filter_by(id=id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(RestaurantByID, '/restaurants/<int:id>')
+
+
+
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
